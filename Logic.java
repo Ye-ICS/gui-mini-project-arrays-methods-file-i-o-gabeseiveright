1adf128da3e52d381ff1f3d2379743a1d9e67418
gabeseiveright <seiverighgm@limestone.on.ca>
2025-12-05 12:06:13 -0500

    changed on/off to numbers, added input action and win condition

--- Logic.java
+++ Logic.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,6 +6,7 @@ public class Logic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String answer;
+        boolean win = false;
         System.out.println("Welcome to lights out, you must turn out all of the lights. Ready to play? (yes/no)");
         answer = sc.nextLine();
         if (answer.equalsIgnoreCase("yes")) {
@@ -16,16 +18,25 @@ public class Logic {
                 for (int k = 0; k < lightColour.length; k++) {
                     randomNum = Math.random();
                     if (randomNum > 0.5) {
-                        System.out.print("ON  ");
+                        System.out.print("1  ");
                     } else {
-                        System.out.print("OFF  ");
+                        System.out.print("0  ");
                     }
                 } 
                 System.out.println();
             }
-            System.out.println("Enter a light you want to switch using coordinate format (eg. (x y) )");
+            int[] guess = new int [2];
             
-
+            while (!win) {
+                System.out.println("Enter a light you want to switch using coordinate format (eg. (y x) )");
+                guess[0] = sc.nextInt();
+                guess[1] = sc.nextInt();
+                onInput(lightColour, guess);
+                checkForWin(lightColour, win);
+                newMap(lightColour, guess);
+            }
+            System.out.println("You win! congrats");
+            System.exit(0);
 
         } else if (answer.equalsIgnoreCase("no")) {
             System.out.println("Goodbye then");
@@ -37,14 +48,45 @@ public class Logic {
 
     }
 
-    static void newMap(int[][] lightColour, double randomNum) {
+    static void checkForWin (int [][] lightColour, boolean win) {
+        if (Arrays.asList(lightColour).contains(1)){
+            win = false;
+        } else {
+            win = true;
+        }
+    }
+
+    static void onInput(int [][] lightColour, int[] guess) {
+        int indexX = guess[0] - 1;
+        int indexY = guess[1] - 1;
+        lightColour[indexX][indexY] = 1 - lightColour[indexX][indexY];
+        if (indexY < 0) {
+            lightColour[indexX][indexY - 1] = 1 - lightColour[indexX][indexY - 1];
+        }
+        if (indexY > 4) {
+            lightColour[indexX][indexY + 1] = 1 - lightColour[indexX][indexY + 1];
+        }
+        if (indexX < 0) {
+            lightColour[indexX - 1][indexY] = 1 - lightColour[indexX - 1][indexY];
+        }
+        if (indexX > 4) {
+            lightColour[indexX + 1][indexY] = 1 - lightColour[indexX + 1][indexY];
+        }
+
+    }
+    
+    static void newMap(int[][] lightColour, int[] guess) {
         for (int i = 0; i < lightColour.length; i++) {
             for (int k = 0; k < lightColour.length; k++) {
-            System.out.println(lightColour[i][k] + " ");
+                double randomNum = lightColour[i][k];
+                if (randomNum > 0.5) {
+                    System.out.print("1  ");
+                } else {
+                    System.out.print("0  ");
+                }
             } 
             System.out.println();
         }
-        System.out.println("Enter a light you want to switch using coordinate format (eg. (x y) )");
     }
 }
 
